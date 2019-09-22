1b8feb4a7be44489edae9172e30ffe807377b042
rorymmclean <rory.mclean@verizon.net>
2019-09-21 21:50:57 -0400

    basic linechart

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET Userlist page. */
+/*
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+      res.render('userlist', {
+          "userlist" : docs
+      });
+  });
+});
+*/
+
 module.exports = router;
