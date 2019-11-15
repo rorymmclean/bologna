52748031c241239bfe42e2590d7a8d3cc359bd74
rorymmclean <rory.mclean@verizon.net>
2019-11-14 21:43:46 -0500

    shell scripts

--- test_8.js
+++ test_8.js
@@ -4,14 +4,12 @@ var spawnSync = require('child_process').spawnSync
 var testloop = process.argv[2]
 if (testloop == null) { testloop = 1 }
 
-spawnSync('ls', ['/mnt/mnt1/master_s', '|', 'while', 'read', 'FILENAME;' +
-    'do', 'cp', '"/mnt/mnt1/master_s/$FILENAME"', '"/mnt/mnt2/temp_'+testloop+'_$FILENAME";', 'done'],
+spawnSync('bash', ['copy1.sh', testloop],
     { stdio: [null, process.stdout, process.stderr] }
   )
-<!--
+
 while (1) {
-  spawnSync('find', ['/mnt/mnt2/', '-maxdepth', '1', '-name', '"temp_'+testloop+'_master*"', '-exec', 'cp', '{}', '/mnt/mnt1', '\;'],
+  spawnSync('bash', ['copy2.sh', testloop],
     { stdio: [null, process.stdout, process.stderr] }
   )
 }
--->
